5862e70b4e6db207683333ebee1ec2b4e89612ef
jamil-rahman <78928081+jamil-rahman@users.noreply.github.com>
2021-02-12 14:38:46 +0600

    updating the script

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const video = document.getElementById("video");
 
 Promise.all([
-   faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
-   faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
-   faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
-   faceapi.nets.faceExpressionNet.loadFromUri('/models'),
+   faceapi.nets.tinyFaceDetector.loadFromUri('/Face-Scanner/models'),
+   faceapi.nets.faceLandmark68Net.loadFromUri('/Face-Scanner/models'),
+   faceapi.nets.faceRecognitionNet.loadFromUri('/Face-Scanner/models'),
+   faceapi.nets.faceExpressionNet.loadFromUri('/Face-Scanner/models'),
 ]).then(startVideo)
 
 function startVideo(){
